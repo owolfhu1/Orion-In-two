b07409f6ae6f41665fb574161fdf0eac8e428968
none <none>
2017-07-05 15:17:49 -0400

    working bones

---
+++ src/main/java/com/example/demo/Orionin2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Orionin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orionin2Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,46 @@
+package com.example.demo.services;
+
+
+import com.example.demo.Repositories.RoleRepository;
+import com.example.demo.Repositories.UserRepository;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public void saveUser(User user) {
+        user.setRoles(Arrays.asList(roleRepository.findByRole("USER")));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        userRepository.save(user);
+    }
+    public void saveAdmin(User user) {
+        user.setRoles(Arrays.asList(roleRepository.findByRole("ADMIN")));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
+        userRepository.save(user);
+    }
+}
